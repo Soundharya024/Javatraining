af0aa9a361891e5e3b5f670c9fdbb3cf3a7457a8
Soundharya <you@example.com>
2025-02-03 15:46:55 +0530

    my first commit

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Hello {
+
+    public static void main(String[] args) {
+        /*scanner scan = new scanner{system.in};
+        System.out.println("enter a number:");
+        int a = scan.nextInt();*/
+        int a = 6&1;
+        System.out.println(a);
+
+    
+    }
+}
